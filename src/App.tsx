6d09c5bb3bc4e04ce5273bd9715a8d53032943e3
Rachel Sebastian <rsebastian@agl.com.au>
2019-11-07 23:07:05 +1100

    intial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+// import logo from './logo.svg';
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://i2.au.reastatic.net/170x32/3015ba9710c7e3ddc2ac30f45fd7906df5b04e442a7f6948f75a6029b8b871e2/main.gif" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
